7069397f6cb285dcd6c4c24797084ad14fc71a85
Cleve <gomescleve@gmail.com>
2015-10-28 12:46:45 +0400

    My First Commit

---
+++ app/src/main/java/gomescleve/com/stormy/AlertDialogFragment.java
@@ -0,0 +1,25 @@
+package gomescleve.com.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Developer1 on 26/10/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_ok_button_text,null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
