4a1d3a9136080e33e747c9742be2e110409f294b
Cleve <gomescleve@gmail.com>
2015-12-16 17:45:53 +0400

    notification added

--- app/src/main/java/gomescleve/com/stormy/UI/StormyActivity.java
+++ app/src/main/java/gomescleve/com/stormy/UI/StormyActivity.java
@@ -4,11 +4,13 @@ package gomescleve.com.stormy.UI;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -156,22 +158,28 @@ public class StormyActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             mTValue = mTemperature.getTemperatureSensor();
-                            mHValue  = mHumidity.getHumiditySensor();
                             mPValue = mPressure.getPressureSensor();
+                            mHValue= mHumidity.getHumiditySensor();
 
-                            Intent intent = new Intent();
-                            PendingIntent pIntent = PendingIntent.getActivity(mContext, 0, intent, 0);
-                            Notification noti = new Notification.Builder(mContext)
-                                    .setTicker("Local Sensors")
-                                    .setContentTitle("Room Temp is "+mTValue+" C")
-                                    .setContentText("Room Temperature: "+mTValue+" C"+" <br/>Room Pressure: "+mPValue+" mBar <br/>Room Humidity: "+mHValue+" %")
-                                    .setSmallIcon(R.drawable.ic_launcher)
-                                    .setContentIntent(pIntent).getNotification();
 
-                            noti.flags = Notification.FLAG_AUTO_CANCEL;
 
-                            NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-                            notificationManager.notify(0,noti);
+                            NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+                            builder.setSmallIcon(R.drawable.ic_launcher);
+                            builder.setContentTitle("Room Temp is " + mTValue+" C");
+                            builder.setContentText("Pressure: " + mPValue + " mBar, Humidity: "+mHValue+" %");
+
+
+
+                            Intent intent = new Intent(mContext,LocalActivity.class);
+                            TaskStackBuilder stackBuilder = TaskStackBuilder.create(mContext);
+                            stackBuilder.addParentStack(LocalActivity.class);
+                            stackBuilder.addNextIntent(intent);
+                            PendingIntent pendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+                            builder.setContentIntent(pendingIntent);
+
+                            NotificationManager NM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                            NM.notify(0,builder.build());
+
 
                         }
                     });
